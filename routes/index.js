64e9fb3199bb3e9be5f6c60073346c2227106019
Kiruthik Kumar <122508631+Kiruthikk07@users.noreply.github.com>
2025-06-23 15:14:40 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const router = express.Router();
 
-const tenantRoutes = require('./tenants');
 const userRoutes = require('./users');
+const tenantRoutes = require('./tenants');
 const deviceRoutes = require('./devices');
 const appRoutes = require('./apps');
+const notificationRoutes = require('./notifications');
+const planRoutes = require('./plans');
+const billingRoutes = require('./billing');
 
-router.use('/tenants', tenantRoutes);
+// Public routes
 router.use('/users', userRoutes);
+router.use('/tenants', tenantRoutes);
 router.use('/devices', deviceRoutes);
 router.use('/apps', appRoutes);
+router.use('/notifications', notificationRoutes);
+router.use('/plans', planRoutes);
+router.use('/billing', billingRoutes);
 
-module.exports = router;
+module.exports = router; 
